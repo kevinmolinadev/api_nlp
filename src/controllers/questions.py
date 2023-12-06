2adf447ba07316c00e71f81089c041d86f80468b
Kevin Molina <kevinmolina.dev@gmail.com>
2023-12-05 22:53:48 -0400

    Deleted development logs and improve query not found messages

--- src/controllers/questions.py
+++ src/controllers/questions.py
@@ -6,11 +6,9 @@ from src.helpers.context import extract_context
 
 def answer_question(question):
     keywords = extract_keywords(question)
-    if keywords is False: return {"question":question,"result":"Por favor formule una pregunta adecuada"}
-    print(keywords)
+    if keywords is False: return {"question":question,"result":"😕 ¡Ups! No entendí bien la pregunta. ¿Podrías reformularla, por favor?"}
     context = validate_context(keywords)
-    if context == "": return {"question":question,"result":"La pregunta no esta disponible, gracias por las nuevas sugerencias"}
-    print(context)
+    if context == "": return {"question":question,"result":"🌟 ¡Gracias por tu pregunta! Sin embargo, no tengo información sobre ese tema. ¿Puedes preguntar algo diferente?"}
     return result_to_question(question,context)
 
 def validate_context(keywords):
@@ -20,8 +18,6 @@ def validate_context(keywords):
 def result_to_question(question,context):
     predictive = Predictive("mrm8488/distill-bert-base-spanish-wwm-cased-finetuned-spa-squad2-es").get_model()
     results  =  predictive({'question': question,'context':context})
-    for item in results:
-        print(item['answer'])
     # Sacamos la diferencia entre start y end, ordenamos en base a la direnrecia mas alta
     best_result  =  max(results, key = lambda x: x["end"] - x["start"])
     return {"question":question,"result":best_result['answer']}
