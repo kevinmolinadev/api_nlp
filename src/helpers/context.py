2e7a4403f6f56e92e75066e4c258c221ada1dde5
Kevin Molina <kevinmolina.dev@gmail.com>
2024-04-15 09:08:44 -0400

    Add new validations in context

--- src/helpers/context.py
+++ src/helpers/context.py
@@ -2,7 +2,7 @@ import re
 """""
 this method receives an array with the contexts, it also receives the keyword to filter the contexts for the specific questions 
 ```python
-    data = ['first context','second context']
+    data = ['first context','second context','second context','second context','second context','second context']
     keywords = ['first']
     extract_context(data,keywords) // ==> 'first context'
 ```
@@ -11,7 +11,9 @@ def extract_context(data, keywords):
     context = []
     for item in data:
         lower_question = str(item.question).lower()
+        #4 estudio ingeniería sistemas informáticos
         keyword_matches = sum(keyword in lower_question for keyword in keywords)
         if keyword_matches == 0: continue
-        context.append(f"{str(item.context).strip()}")
+        if (keyword_matches>=int(len(keywords)/2)):
+            context.append(f"{str(item.context).strip()}")
     return context
